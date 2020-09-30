db0342beda01edb8b65f6e79322ca3f8497e5e5f
RanikaR <rmr29@njit.edu>
2020-09-30 16:12:05 -0400

    uncommented component.set() and added demonstration explanation

--- ExpensesApp/ExpensesApp/force-app/main/default/aura/expenses/expensesHelper.js
+++ ExpensesApp/ExpensesApp/force-app/main/default/aura/expenses/expensesHelper.js
@@ -8,7 +8,11 @@
         // logging statement to see what component.set does
         console.log("Expenses before 'create': " + JSON.stringify(theExpenses));
         theExpenses.push(newExpense);
-        // component.set("v.expenses", theExpenses);
+        component.set("v.expenses", theExpenses);
         console.log("Expenses after 'create': " + JSON.stringify(theExpenses));
+
+        // component.set() does affect theExpenses, but it triggers notification that
+        // the expenses attribute has changed. In order to trigger underlying effects
+        // built into the Aura Components programming model, you need to use component.set()
     }
 })
