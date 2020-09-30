2120a3a79a532d5371aa1b28782bd249af774fec
RanikaR <rmr29@njit.edu>
2020-09-30 19:11:26 -0400

    campingApp progress

--- ExpensesApp/ExpensesApp/force-app/main/default/aura/campingList/campingListController.js
+++ ExpensesApp/ExpensesApp/force-app/main/default/aura/campingList/campingListController.js
@@ -1,5 +1,24 @@
 ({
-	myAction : function(component, event, helper) {
+    clickCreateItem: function(component, event, helper) {
+        let validItem = component.find('campinglistform').reduce(function (validSoFar, inputCmp) {
+            // Displays error messages for invalid fields
+            inputCmp.showHelpMessageIfInvalid();
+            return validSoFar && inputCmp.get('v.validity').valid;
+		}, true);
 		
-	}
+        // If we pass error checking, do some real work
+        if(validItem){
+            // Create the new expense
+            let newItem = component.get("v.newItem");
+			console.log("Create item: " + JSON.stringify(newItem));
+			var items = component.get("v.items");
+			var item = JSON.parse(JSON.stringify(newItem));
+
+			items.push(item);
+
+			component.set("v.items", items);
+			component.set("v.newItem", { 'sobjectType': 'Camping_Item__c','Name': '','Quantity__c': 0,
+			'Price__c': 0,'Packed__c': false });
+        }
+    }
 })
