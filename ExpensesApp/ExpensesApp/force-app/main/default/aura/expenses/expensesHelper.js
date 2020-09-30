887d6820ab634a3baef83c6d7a9f59ac75d44301
RanikaR <rmr29@njit.edu>
2020-09-30 16:07:36 -0400

    Added logging statements to see what component.set does in the expensesHelper

--- ExpensesApp/ExpensesApp/force-app/main/default/aura/expenses/expensesHelper.js
+++ ExpensesApp/ExpensesApp/force-app/main/default/aura/expenses/expensesHelper.js
@@ -4,7 +4,11 @@
         // Copy the expense to a new object
         // THIS IS A DISGUSTING, TEMPORARY HACK
         let newExpense = JSON.parse(JSON.stringify(expense));
+        
+        // logging statement to see what component.set does
+        console.log("Expenses before 'create': " + JSON.stringify(theExpenses));
         theExpenses.push(newExpense);
         component.set("v.expenses", theExpenses);
+        console.log("Expenses after 'create': " + JSON.stringify(theExpenses));
     }
 })
