94a184410beae85f77075da053b4338883b50fab
RanikaR <rmr29@njit.edu>
2020-10-01 13:29:31 -0400

    Camping app updated to use Camping List Controller

--- ExpensesApp/ExpensesApp/force-app/main/default/aura/campingList/campingListController.js
+++ ExpensesApp/ExpensesApp/force-app/main/default/aura/campingList/campingListController.js
@@ -1,24 +1,32 @@
 ({
-    clickCreateItem: function(component, event, helper) {
-        let validItem = component.find('campinglistform').reduce(function (validSoFar, inputCmp) {
-            // Displays error messages for invalid fields
-            inputCmp.showHelpMessageIfInvalid();
-            return validSoFar && inputCmp.get('v.validity').valid;
-		}, true);
-		
-        // If we pass error checking, do some real work
-        if(validItem){
-            // Create the new expense
-            let newItem = component.get("v.newItem");
-			console.log("Create item: " + JSON.stringify(newItem));
-			var items = component.get("v.items");
-			var item = JSON.parse(JSON.stringify(newItem));
-
-			items.push(item);
-
-			component.set("v.items", items);
-			component.set("v.newItem", { 'sobjectType': 'Camping_Item__c','Name': '','Quantity__c': 0,
-			'Price__c': 0,'Packed__c': false });
+        // Load camping items from Salesforce
+    doInit: function(component, event, helper) {
+        // Create the action
+        let action = component.get("c.getItems");
+        // Add callback behavior for when response is received
+        action.setCallback(this, function(response) {
+            let state = response.getState();
+            if (state === "SUCCESS") {
+                component.set("v.items", response.getReturnValue());
+            }
+            else {
+                console.log("Failed with state: " + state);
+            }
+        });
+        // Send action off to be executed
+        $A.enqueueAction(action);
+    },
+    
+    createItem : function(component, event, helper){
+        
+        helper.validateFields (component,component.find("name"));
+        helper.validateFields (component,component.find("Price"));
+        helper.validateFields (component,component.find("Quantity"));
+        if(component.get("v.er") === false)
+        {     
+            var Item = component.get("v.newItem");            
+            helper.createItem (component,Item);             
+                       
         }
-    }
+	}    
 })
--- ExpensesApp/ExpensesApp/force-app/main/default/aura/campingList/campingListHelper.js
+++ ExpensesApp/ExpensesApp/force-app/main/default/aura/campingList/campingListHelper.js
@@ -1,5 +1,31 @@
 ({
-    helperMethod : function() {
-
-    }
+        validateFields : function (component,field) {
+        
+        var nameField = field;
+        console.log('yes:'+nameField);
+        var expname = nameField.get("v.value"); 
+        if ($A.util.isEmpty(expname)){
+           component.set("v.er",true);
+           nameField.set("v.errors", [{message:"this field can't be blank."}]);
+        }
+        else {
+            nameField.set("v.errors", null);
+        }
+    },
+    
+    createItem: function(component, item) {
+        let action = component.get("c.saveItem");
+        action.setParams({
+            "item": item
+        });
+        action.setCallback(this, function(response){
+            let state = response.getState();
+            if (state === "SUCCESS") {
+                let items = component.get("v.items");
+                items.push(response.getReturnValue());
+                component.set("v.items", items);
+            }
+        });
+        $A.enqueueAction(action);
+    },
 })
