10a84ac67cd456db62acde5f250e9b92b74dad79
RanikaR <rmr29@njit.edu>
2020-10-01 13:29:09 -0400

    Expenses app updated to use ExpensesController

--- ExpensesApp/ExpensesApp/force-app/main/default/aura/expenses/expensesHelper.js
+++ ExpensesApp/ExpensesApp/force-app/main/default/aura/expenses/expensesHelper.js
@@ -1,18 +1,17 @@
 ({
     createExpense: function(component, expense) {
-        let theExpenses = component.get("v.expenses");
-        // Copy the expense to a new object
-        // THIS IS A DISGUSTING, TEMPORARY HACK
-        let newExpense = JSON.parse(JSON.stringify(expense));
-        
-        // logging statement to see what component.set does
-        console.log("Expenses before 'create': " + JSON.stringify(theExpenses));
-        theExpenses.push(newExpense);
-        component.set("v.expenses", theExpenses);
-        console.log("Expenses after 'create': " + JSON.stringify(theExpenses));
-
-        // component.set() does affect theExpenses, but it triggers notification that
-        // the expenses attribute has changed. In order to trigger underlying effects
-        // built into the Aura Components programming model, you need to use component.set()
-    }
+        let action = component.get("c.saveExpense");
+        action.setParams({
+            "expense": expense
+        });
+        action.setCallback(this, function(response){
+            let state = response.getState();
+            if (state === "SUCCESS") {
+                let expenses = component.get("v.expenses");
+                expenses.push(response.getReturnValue());
+                component.set("v.expenses", expenses);
+            }
+        });
+        $A.enqueueAction(action);
+    },
 })
