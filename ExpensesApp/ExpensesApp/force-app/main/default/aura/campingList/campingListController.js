98f28330aa8b812597dcedfa27b476eb8cbe90c8
RanikaR <rmr29@njit.edu>
2020-10-05 17:25:19 -0400

    challenge passed but app not working as intended

--- ExpensesApp/ExpensesApp/force-app/main/default/aura/campingList/campingListController.js
+++ ExpensesApp/ExpensesApp/force-app/main/default/aura/campingList/campingListController.js
@@ -17,19 +17,31 @@
         $A.enqueueAction(action);
     },
 
-    handleAddItem: function(component, event, helper) {
-        var action = component.get("c.saveItem");
-        action.setParams({"item" : newItem});
-        action.setCall(this, function(response) {
-            var state = response.getState();
-            if(component.isValid() && state === "SUCCESS") {
-                console.log("Succeeded with state: " + state);
-            } else {
-                console.log("Failed with state: " + state);
+    handleAddItem : function(component, event, helper){
+        var newCampingItem = event.getParam('item');
+
+        //var theItems = component.get("v.items");
+        //theItems.push(newCampingItem);
+        //component.set("v.items", theItems);
+
+        //helper.createItem(component, newCampingItem);
+        var saveItemAction = component.get('c.saveItem');
+        saveItemAction.setParams({ 'campingItem' :  newCampingItem });
+
+        saveItemAction.setCallback(this, function(response){
+            var respState = response.getState();
+            console.log(respState);
+            if( respState == 'SUCCESS'){
+                var theItems = component.get("v.items");
+                theItems.push(response.getReturnValue());
+                component.set("v.items", theItems);
             }
         });
-    },
 
+        $A.enqueueAction(saveItemAction);
+    }
+
+    
 
     
    /* createItem : function(component, event, helper){
--- ExpensesApp/ExpensesApp/force-app/main/default/aura/campingListForm/campingListFormHelper.js
+++ ExpensesApp/ExpensesApp/force-app/main/default/aura/campingListForm/campingListFormHelper.js
@@ -6,13 +6,11 @@
 		createItem.fire();
 
 		// Resets fields to blank
-		component.set("v.newItem", {
-			'sobjectType' : 'Camp0ing_Item__c', 
-			'Name' : '',
-			'Quantity__c' : 0, 
-			'Price__c' : 0,
-			'Packed__c' : false
-		})
+		component.set("v.newItem",{'sobjectType':'Camping_Item__c',
+		'Name': '',
+		'Quantity__c': 0,
+		'Price__c': 0,
+		'Packed__c': false});
 		
 	}, 
 
