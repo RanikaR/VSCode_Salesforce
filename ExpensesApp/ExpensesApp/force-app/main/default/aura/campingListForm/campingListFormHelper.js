c4cee6c3f5966384e65c381d4c3e5f5a7d4fd688
RanikaR <rmr29@njit.edu>
2020-10-05 16:29:55 -0400

    replaced createItem with clickCreateItem

--- ExpensesApp/ExpensesApp/force-app/main/default/aura/campingListForm/campingListFormHelper.js
+++ ExpensesApp/ExpensesApp/force-app/main/default/aura/campingListForm/campingListFormHelper.js
@@ -1,5 +1,19 @@
 ({
-	helperMethod : function() {
+	createItem : function(component, newItem) {
+
+		var createItem = component.getItem("createItem");
+		createItem.setParams({"item":item});
+		createItem.fire();
+
+		component.set("v.newItem", {
+			'sobjectType' : 'Camp0ing_Item__c', 
+			'Name' : '',
+			'Quantity__c' : 0, 
+			'Price__c' : 0,
+			'Packed__c' : false
+		})
 		
-	}
+	}, 
+
+	
 })
