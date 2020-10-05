ec1411034de1540241ce39b6f5597f6019ee079b
RanikaR <rmr29@njit.edu>
2020-10-05 16:51:02 -0400

    added validations

--- ExpensesApp/ExpensesApp/force-app/main/default/aura/campingListForm/campingListFormHelper.js
+++ ExpensesApp/ExpensesApp/force-app/main/default/aura/campingListForm/campingListFormHelper.js
@@ -5,6 +5,7 @@
 		createItem.setParams({"item":item});
 		createItem.fire();
 
+		// Resets fields to blank
 		component.set("v.newItem", {
 			'sobjectType' : 'Camp0ing_Item__c', 
 			'Name' : '',
@@ -15,5 +16,31 @@
 		
 	}, 
 
-	
+	validateItemForm : function(component) {
+		var validItem = true;
+
+		var nameField = component.find("itemname");
+		var itemname = nameField.get("v.value");
+
+		if ($A.util.isEmpty(quantity)){
+            validItem = false;
+            quantityField.set("v.errors", [{message:"Quantity can't be blank."}]);
+        }
+        else {
+            quantityField.set("v.errors", null);
+        }
+		// Price must not be blank
+        var priceField = component.find("price");
+        var price = priceField.get("v.value");
+        if ($A.util.isEmpty(price)){
+            validItem = false;
+            priceField.set("v.errors", [{message:"Price can't be blank."}]);
+        }
+        else {
+            quantityField.set("v.errors", null);
+        }
+            return validItem;
+	}
+
+
 })
