713096b044b3e225aafcf939830ccaf4bc749b4f
RanikaR <rmr29@njit.edu>
2021-05-18 14:24:16 -0400

    added notes

--- test-lwc/test-lwc/force-app/main/default/lwc/unitTest/__tests__/sum.test.js
+++ test-lwc/test-lwc/force-app/main/default/lwc/unitTest/__tests__/sum.test.js
@@ -5,3 +5,18 @@ describe('sum()', () => {
         expect(sum(1, 2)).toBe(3);
     });
 });
+
+/* 
+ln 1 - imports exported sum function from sum.js file
+ln 3 - start of the Jest test suite. describe function accepts two arguments. 
+    the first is the description of the unit being tested.
+    second is the callback function that holds test(s)
+ln 4 - the test. "it" is an alias for test. the "it" function accepts two arguments.
+    first - description of what is expected
+    second - callback function that builds up the test and holds the assertions 
+    or expectations for the test
+ln 5 - expect statement asserting the success condition. toBe is a Jest matcher
+
+More assertions can be inserted following line 5, including using different matchers. 
+expect(sum(1, 2)).not.toBeGreaterThan(3); - ensures value is not greater than 3.
+*/ 
