14ecd1feebb866bbe5a05949598e54ff653892b2
RanikaR <rmr29@njit.edu>
2021-05-21 09:01:23 -0400

    added moduleNameMapper

--- test-lwc/test-lwc/jest.config.js
+++ test-lwc/test-lwc/jest.config.js
@@ -2,4 +2,7 @@ const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 module.exports = {
     ...jestConfig,
     // add any custom configurations here
+    moduleNameMapper: {
+        '^lightning/button$': '<rootDir>/force-app/test/jest-mocks/lightning/button'
+    }
 };
