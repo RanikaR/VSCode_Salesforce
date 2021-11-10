0e36ab74c4fc9d2d7dad6e7d1e97c61a7e296d3a
RanikaR <rmr29@njit.edu>
2021-11-10 09:16:33 -0500

    updated javascript file

--- LWCForVisualforceDevs/lwcForVisualforceDevs/force-app/main/default/lwc/accountFinder/accountFinder.js
+++ LWCForVisualforceDevs/lwcForVisualforceDevs/force-app/main/default/lwc/accountFinder/accountFinder.js
@@ -1,3 +1,13 @@
 import { LightningElement } from 'lwc';
 
-export default class AccountFinder extends LightningElement {}
+export default class AccountFinder extends LightningElement {
+    annualRevenue = null;
+
+    handleChange(event) {
+        this.annualRevenue = event.detail.value;
+    }
+
+    reset() {
+        this.annualRevenue = null;
+    }
+}
