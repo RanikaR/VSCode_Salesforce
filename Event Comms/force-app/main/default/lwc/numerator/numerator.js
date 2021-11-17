4ef54258fe7ac251c526ec3000866d087f9f9884
RanikaR <rmr29@njit.edu>
2021-11-17 10:44:44 -0500

    created counter property and methods

--- Event Comms/force-app/main/default/lwc/numerator/numerator.js
+++ Event Comms/force-app/main/default/lwc/numerator/numerator.js
@@ -2,5 +2,12 @@ import { LightningElement } from 'lwc';
 
 export default class Numerator extends LightningElement {
 
-    
+    counter = 0;
+    handleIncrement() {
+        this.counter++;
+    }
+
+    handleDecrement(){
+        this.counter--;
+    }
 }
