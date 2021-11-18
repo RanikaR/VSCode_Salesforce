0252cb4f1bbe0414af02065edce01d227b11446f
RanikaR <rmr29@njit.edu>
2021-11-18 11:30:23 -0500

    added factors property

--- Event Comms/force-app/main/default/lwc/controls/controls.js
+++ Event Comms/force-app/main/default/lwc/controls/controls.js
@@ -1,6 +1,9 @@
 import { LightningElement } from 'lwc';
 
 export default class Controls extends LightningElement {
+
+    factors = [0,2,3,4,5,6];
+
     handleAdd() {
         console.log("handleAdd event dispatched");
         this.dispatchEvent(new CustomEvent('add'));
