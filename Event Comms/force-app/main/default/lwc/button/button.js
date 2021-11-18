6a6bee1a7ab172196f999e77ca3e87fab6808369
RanikaR <rmr29@njit.edu>
2021-11-18 09:32:22 -0500

    createdhandleButton() method

--- Event Comms/force-app/main/default/lwc/button/button.js
+++ Event Comms/force-app/main/default/lwc/button/button.js
@@ -1,3 +1,13 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class Button extends LightningElement {}
+export default class Button extends LightningElement {
+    @api label;
+    @api icon;
+
+    handleButton(event) {
+        this.dispatchEvent(new CustomEvent('buttonclick', {
+            // bubbles:true
+        }));
+        console.log("buttonclick event dispatched");
+    }
+}
