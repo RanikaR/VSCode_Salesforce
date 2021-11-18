ff774a6143cb4dbe1631857726ad93c18e6dc3af
RanikaR <rmr29@njit.edu>
2021-11-18 08:49:43 -0500

    added handleMultiple method

--- Event Comms/force-app/main/default/lwc/controls/controls.js
+++ Event Comms/force-app/main/default/lwc/controls/controls.js
@@ -5,8 +5,18 @@ export default class Controls extends LightningElement {
         console.log("handleAdd event dispatched");
         this.dispatchEvent(new CustomEvent('add'));
     }
+    
     handleSubtract() {
         console.log("handleSubtract event dispatched");
         this.dispatchEvent(new CustomEvent('subtract'));
+    }
+    
+    handleMultiple(event) {
+        const factor = event.target.dataset.factor;
+        console.log("data factor = " + factor);
+        console.log("handleMultiple event dispatched");
+        this.dispatchEvent(new CustomEvent('multiply', {
+            detail:factor
+        }));
     }
 }
